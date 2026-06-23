4ad7dbd62dce2b0345dd6ce009d317cee3cac528
joanwwy <joanwwy@yahoo.com>
2026-06-23 08:33:39 +0000

    test

--- server.js
+++ server.js
@@ -16,13 +16,14 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 // Needed for Prisma to connect to database
-const { Pool } = require('pg');
-const { PrismaPg } = require('@prisma/adapter-pg');
+//const { Pool } = require('pg');
+//const { PrismaPg } = require('@prisma/adapter-pg');
 const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
 
-const pool = new Pool({ connectionString: process.env.DATABASE_URL });
-const adapter = new PrismaPg(pool);
-const prisma = new PrismaClient({ adapter });
+//const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+//const adapter = new PrismaPg(pool);
+//const prisma = new PrismaClient({ adapter });
 
 // Main landing page
 app.get('/', async function(req, res) {
