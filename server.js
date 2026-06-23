266198c8b2b68d998dd74a8d772c0d3f3324a402
joanwwy <joanwwy@yahoo.com>
2026-06-23 08:44:21 +0000

    test

--- server.js
+++ server.js
@@ -19,7 +19,6 @@ app.use(express.urlencoded({extended: true}));
 const { Pool } = require('pg');
 const { PrismaPg } = require('@prisma/adapter-pg');
 const { PrismaClient } = require('@prisma/client');
-//const prisma = new PrismaClient();
 
 const pool = new Pool({ connectionString: process.env.DATABASE_URL });
 const adapter = new PrismaPg(pool);
