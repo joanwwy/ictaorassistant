8c3e8ef7697647607bf52e6f87429a05ced32e1d
joanwwy <joanwwy@yahoo.com>
2026-06-24 04:03:21 +0000

    ok

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, UploadFile, File, Form
 from fastapi.middleware.cors import CORSMiddleware
 from langchain_experimental.text_splitter import SemanticChunker
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
-from langchain.schema import HumanMessage, SystemMessage
+from langchain_core.messages import HumanMessage, SystemMessage
 
 app = FastAPI()
 
@@ -15,18 +15,14 @@ app.add_middleware(
 
 @app.post("/process")
 async def process(query: str = Form(...), file: UploadFile = File(...)):
-    # Read the uploaded file
     contents = await file.read()
     text = contents.decode("utf-8")
 
-    # Semantic chunking
     splitter = SemanticChunker(OpenAIEmbeddings())
     docs = splitter.create_documents([text])
 
-    # Combine chunks into context
     context = "\n\n".join([doc.page_content for doc in docs])
 
-    # Query using ChatOpenAI directly
     llm = ChatOpenAI(temperature=0)
     messages = [
         SystemMessage(content="You are a helpful assistant. Use the following document to answer the user's question.\n\n" + context),
@@ -34,4 +30,4 @@ async def process(query: str = Form(...), file: UploadFile = File(...)):
     ]
     response = llm.invoke(messages)
 
-    return { "result": response.content }
+    return {"result": response.content}
